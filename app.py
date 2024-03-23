5c57cfad5451ba7ca49280bbc0cc14b3e00d1b5c
Muhammadzainattiq <zainatteeq@gmail.com>
2024-03-23 14:15:57 +0500

    minor changes

--- app.py
+++ app.py
@@ -14,16 +14,19 @@ created_style = """
 header_style = """
     text-align: center;
     color: white;
-    background-color: #0080FF;
+    background-color: #800080;
     padding: 10px;
     border-radius: 10px;
     margin-bottom: 30px;
 """
 
 
-@st.cache_data
-def generate_response(topic):
-    content = '''You are a quiz generator. You will be given a particular topic and you will give in response  Mulitple Choice Questions on that topic. Your response should be in the JSON format (a list of dictionaries with each dictionary with six key-value pairs: the question, option1, option2, option3, option4 and correct_option as follows:
+@st.cache_data(show_spinner=False)
+def generate_response(topic, number, difficulty):
+    system_content = '''You are a quiz generator. You will be given a particular topic and you will give in response some number of Mulitple Choice Questions on that topic of specific difficulty level. The difficulty level will be "easy", "medium" "difficult" or "hardest". The user will give in input the topic, the number of questions and the difficulty level. The format of user  prompt will be as follows:
+    {topic: "Artificial Intellisense" , number : 10, difficulty = "easy" }
+
+    And Your response should be in the JSON format (a list of dictionaries with each dictionary with six key-value pairs: the question, option1, option2, option3, option4 and correct_option as follows:
          [
          {
             "question": "Which is the correct answer?",
@@ -41,29 +44,37 @@ def generate_response(topic):
             "correct": "Correct answer"}...
          ]
          '''
+    user_content = {f"topic: \"{topic}\", number = \"{number}\", difficulty = \"{difficulty}\""}
+    user_content = str(user_content)
     messages = [
-        {'role': 'system', 'content': content},
-        {'role': 'user', 'content': f'{topic}'}
+        {'role': 'system', 'content': system_content},
+        {'role': 'user', 'content': user_content}
     ]
     response = openai.chat.completions.create(model='gpt-3.5-turbo-0125', messages=messages)
     return response
 
 def main():
-    # Check if response is already stored in session state
     st.set_page_config(page_title="Quiz APP", page_icon="❓")
     st.markdown("<p style='{}'>➡️created by 'Muhammad Zain Attiq'</p>".format(created_style), unsafe_allow_html=True)
     st.markdown(f"<h1 style='{header_style}'>Dynamic Quiz App</h1>", unsafe_allow_html=True)
+    # Check if response is already stored in session state
     if 'response' not in st.session_state:
         st.session_state.response = None
 
-    topic = st.text_input("Enter the topic: ")
+    topic = st.text_input("Enter the topic of the questions: ")
+    number = st.number_input("Enter the number of questions you want: (Max 10)", 1, 10)
+    difficulty = st.selectbox("Select the difficulty level: ", ["Easy", "Medium", "Difficult", "Hardest"])
     # number = st.slider("Number of MCQs",1,20,2)
-    if st.button("Generate"):
-        # Generate and store response in session state
-        response = generate_response(topic)
-        response = response.choices[0].message.content
-        st.session_state.response = json.loads(response)
 
+    if st.button("Generate"):
+        if topic:
+        # Generate and store response in session state
+            st.spinner("Generating MCQs...")  
+            response = generate_response(topic, number, difficulty)
+            response = response.choices[0].message.content
+            st.session_state.response = json.loads(response)
+        else:
+            st.warning("Please enter a topic.")
     # If response is available, display the quiz
     if st.session_state.response:
         marks = 0
@@ -88,7 +99,7 @@ def main():
                     st.error(result)
 
         with st.expander("Check the results: "):
-            st.subheader(f"You have got {marks} marks out of 10.")
+            st.subheader(f"You have got {marks} marks out of {number}.")
 
 if __name__ == "__main__":
     main()  
